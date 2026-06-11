dd41ed611183dcc805a8eb9ad00017a1856c0c2f
ghostik <karenheister5@gmail.com>
2026-06-11 16:19:44 +0300

    feat: add create, findAll, findAllWhere, findOne methods

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,7 +1,55 @@
-import { Controller } from '@nestjs/common';
-import { CatsService } from './cats.service';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Query,
+  ValidationPipe
+} from '@nestjs/common'
+import { CatsService } from './cats.service'
+import { CreateCatDto } from './dto/requests/create-cat.dto'
+import { ApiOperation } from '@nestjs/swagger'
+import { FindCatsWhereDto } from './dto/requests/find-cats-where.dto'
 
 @Controller('cats')
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
+
+  @ApiOperation({
+    summary: 'Create cat',
+    description: 'Create cat'
+  })
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() createCatDto: CreateCatDto) {
+    return this.catsService.create(createCatDto)
+  }
+
+  @ApiOperation({
+    summary: 'Get all cats list',
+    description: 'Get all cats list'
+  })
+  @Get()
+  @HttpCode(HttpStatus.OK)
+  findAll() {
+    return this.catsService.findAll()
+  }
+
+  @Get('where')
+  findAllWhere(@Query(ValidationPipe) query: FindCatsWhereDto) {
+    return this.catsService.findAllWhere(query.age, query.breed)
+  }
+
+  @ApiOperation({
+    summary: 'Get cat with id',
+    description: 'Get cat with id'
+  })
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.catsService.findWithId(id)
+  }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,4 +1,48 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+
+export interface createCatRequest {
+  name: string
+  age: number
+  breed: string
+}
+
+export interface Cat {
+  id: number
+  name: string
+  age: number
+  breed: string
+}
 
 @Injectable()
-export class CatsService {}
+export class CatsService {
+  private cats: Cat[] = []
+  private id = 1
+
+  public create(data: createCatRequest): Cat {
+    const newCat: Cat = {
+      id: this.id++,
+      ...data
+    }
+
+    this.cats.push(newCat)
+
+    return newCat
+  }
+
+  public findAll(): Cat[] {
+    return this.cats
+  }
+
+  public findWithId(id: number): Cat | undefined {
+    return this.cats.find((cat) => cat.id === id)
+  }
+
+  public findAllWhere(age?: number, breed?: string): Cat[] {
+    return this.cats.filter((cat) => {
+      const matchAge = age !== undefined ? cat.age === age : true
+      const matchBreed = breed !== undefined ? cat.breed === breed : true
+
+      return matchAge && matchBreed
+    })
+  }
+}
