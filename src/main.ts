37878df0753f501088f2990e36dff2f7b8993df3
ghostik <karenheister5@gmail.com>
2026-06-11 14:03:56 +0300

    feat: enabled the OpenAPI documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create(AppModule)
+
+  // Беру уже созданный экземпляр сервиса из контейнера приложения
+  const config = app.get(ConfigService)
+  const logger = new Logger()
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Cats')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build()
+  const documentFactory = () => SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('/docs', app, documentFactory)
+
+  const host = config.getOrThrow<string>('HTTP_HOST')
+  const port = config.getOrThrow<number>('HTTP_PORT')
+
+  await app.listen(port ?? 3000);
+
+  logger.log(`App started on ${host}`)
+  logger.log(`Swagger: ${host}/docs`)
 }
-bootstrap();
+bootstrap()
