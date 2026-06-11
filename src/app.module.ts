25dca00f1ce4dcf620e625a4e82e7e6997aea266
ghostik <karenheister5@gmail.com>
2026-06-11 14:11:08 +0300

    feat: add cats entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
   imports: [
@@ -9,7 +10,8 @@ import { ConfigModule } from '@nestjs/config';
     // в каждом новом модуле
     ConfigModule.forRoot({
       isGlobal: true
-    })
+    }),
+    CatsModule
   ],
   controllers: [AppController],
   providers: [AppService],
