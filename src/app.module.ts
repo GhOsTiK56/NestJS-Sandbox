e75cd012799465032de388cdbecf97e00d6abbe1
ghostik <karenheister5@gmail.com>
2026-06-11 14:04:58 +0300

    chore: import ConfigModule global

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    // Объявляю подключение конфига глобально, чтобы не импортировать его
+    // в каждом новом модуле
+    ConfigModule.forRoot({
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
