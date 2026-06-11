ba6293adfbf2e391a1622a95cf76b5829ac8eb6f
ghostik <karenheister5@gmail.com>
2026-06-11 16:16:28 +0300

    feat: add request dto to create & find cats

---
+++ src/cats/dto/requests/create-cat.dto.ts
@@ -0,0 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+export class CreateCatDto {
+  @ApiProperty({
+    example: 'Myavka'
+  })
+  name: string
+
+  @ApiProperty({
+    example: 3
+  })
+  age: number
+
+  @ApiProperty({
+    example: 'average'
+  })
+  breed: string
+}
---
+++ src/cats/dto/requests/find-cats-where.dto.ts
@@ -0,0 +1,20 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { Type } from 'class-transformer'
+import { IsInt, IsOptional, IsString } from 'class-validator'
+
+export class FindCatsWhereDto {
+  @ApiProperty({
+    example: 3
+  })
+  @IsOptional()
+  @IsInt()
+  @Type(() => Number)
+  age?: number
+
+  @ApiProperty({
+    example: 'average'
+  })
+  @IsOptional()
+  @IsString()
+  breed?: string
+}
