589ab4eb43000a3776c2cdbaf1380604f506db6e
ghostik <karenheister5@gmail.com>
2026-06-11 14:04:32 +0300

    feat: add healthCheck endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+/*
+Контроллер отвечает за обработку запросов и отправку ответов обрабно клиенту
+*/
+
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('health')
+  public healthCheck() {
+    return this.appService.healthCheck()
   }
 }
