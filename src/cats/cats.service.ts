66193553f0819f4fff03d1ba0db5fe8b26fcfcec
ghostik <karenheister5@gmail.com>
2026-06-12 18:03:17 +0300

    feat: add database operations

--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -1,54 +1,61 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import type { Cat, CreateCatData, UpdateCatData } from './interfaces';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { CreateCatRequest, UpdateCatRequest } from './dto';
+import { Cat } from 'prisma/generated/client';
 
 @Injectable()
 export class CatsService {
-  private cats: Cat[] = [];
-  private id = 1;
+  constructor(private readonly prismaService: PrismaService) {}
 
-  public create(data: CreateCatData): Cat {
-    const newCat: Cat = {
-      id: this.id++,
-      ...data
-    };
-
-    this.cats.push(newCat);
-
-    return newCat;
-  }
-
-  public findAll(): Cat[] {
-    return this.cats;
-  }
-
-  public findWithId(id: number): Cat | undefined {
-    return this.cats.find((cat) => cat.id === id);
-  }
-
-  public findAllWhere(age?: number, breed?: string): Cat[] {
-    return this.cats.filter((cat) => {
-      const matchAge = age !== undefined ? cat.age === age : true;
-      const matchBreed = breed !== undefined ? cat.breed === breed : true;
-
-      return matchAge && matchBreed;
+  public async create(data: CreateCatRequest): Promise<Cat> {
+    const cat = await this.prismaService.cat.create({
+      data
     });
-  }
-
-  public update(id: number, data: UpdateCatData): Cat | undefined {
-    const cat = this.cats.find((cat) => cat.id === id);
-
-    if (!cat) {
-      throw new NotFoundException(`Cat with id: ${id} not found`);
-    }
-
-    // assign мутирует объект и автоматически добавляет в массив
-    Object.assign(cat, data);
 
     return cat;
   }
 
-  public remove(id: number) {
-    this.cats = this.cats.filter((cat) => cat.id !== id);
-    return { message: 'ok' };
+  public async findAll(): Promise<Cat[]> {
+    return await this.prismaService.cat.findMany();
+  }
+
+  public async findWithId(id: number): Promise<Cat[]> {
+    return await this.prismaService.cat.findMany({
+      where: {
+        id
+      }
+    });
+  }
+
+  public async findAllWhere(age?: number, breed?: string): Promise<Cat[]> {
+    return await this.prismaService.cat.findMany({
+      where: {
+        age,
+        breed
+      }
+    });
+  }
+
+  public async update(id: number, data: UpdateCatRequest): Promise<Cat> {
+    return await this.prismaService.cat.update({
+      where: {
+        id
+      },
+      data: {
+        ...data
+      }
+    });
+  }
+
+  public async remove(id: number): Promise<Cat> {
+    return await this.prismaService.cat.delete({
+      where: {
+        id
+      }
+    });
+  }
+
+  public async removeAll() {
+    return await this.prismaService.cat.deleteMany({});
   }
 }
