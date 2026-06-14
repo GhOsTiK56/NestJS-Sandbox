cc2620352c3079a3d5b31769ed9de76831a8bb1b
ghostik <karenheister5@gmail.com>
2026-06-14 15:23:44 +0300

    feat: add auth entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,16 @@ import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { CatsModule } from './cats/cats.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true
     }),
-    PrismaModule,
-    CatsModule
+    AuthModule,
+    CatsModule,
+    PrismaModule
   ],
   controllers: [AppController],
   providers: [AppService]
